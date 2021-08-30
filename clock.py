a31646be5d6b7e4cb4d3fcf8eabbba9f5214cede
vladsuxunun <47049434+vladsuxunun@users.noreply.github.com>
2021-08-29 17:54:21 -0700

    dd

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=70)
+@sched.scheduled_job('interval', minutes=140)
 def timed_job():
     requests.get("https://vkbacktask.herokuapp.com/task1")
     
